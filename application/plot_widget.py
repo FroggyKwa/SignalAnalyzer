81a005c2fffe37ac9d1d79686ab5e1b0154736d1
FroggyKwa <ilya.laguxa@gmail.com>
2023-06-29 04:54:07 +1000

    add check for correct fragment range

--- application/plot_widget.py
+++ application/plot_widget.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QMenu
 from pyqtgraph import PlotWidget, InfiniteLine
 
 from consts import SIGNAL_FRAGMENT_ACTION_TEXT
+from dialogs import open_warning_messagebox
 from utils import show_fragment_dialog
 
 
@@ -20,6 +21,7 @@ class MyPlotWidget(PlotWidget):
         self.line_2 = None
         self.plot_data = plot_data
         self.frequency = frequency
+
     def contextMenuEvent(self, event):
         contextMenu = QMenu(self)
         fragment_action = contextMenu.addAction(SIGNAL_FRAGMENT_ACTION_TEXT)
@@ -34,7 +36,7 @@ class MyPlotWidget(PlotWidget):
             mouse_point = vb.mapSceneToView(scene_coords)
             line = InfiniteLine(angle=90, movable=False, pen='r')
             line.setValue(mouse_point.x())
-            if not self.border_left:
+            if not self.line_1:
                 self.border_left = mouse_point.x()
                 self.line_1 = line
             elif not self.border_right:
@@ -60,8 +62,19 @@ class MyPlotWidget(PlotWidget):
 
     def open_scaled_plot(self, border_left=-1, border_right=-1):
         if border_left != -1 and border_right != -1:
+            if border_left > border_right:
+                open_warning_messagebox('Ошибка!', 'Диапазон значений указан неверно!')
+                return
+            self.removeItem(self.line_1)
+            self.removeItem(self.line_2)
             self.border_left = border_left
             self.border_right = border_right
+            self.line_1 = InfiniteLine(angle=90, movable=False, pen='r')
+            self.line_1.setValue(border_left)
+            self.line_2 = InfiniteLine(angle=90, movable=False, pen='r')
+            self.line_2.setValue(border_right)
+            self.addItem(self.line_1)
+            self.addItem(self.line_2)
 
         data = self.plot_data[1][int(self.border_left * self.frequency):int(self.border_right * self.frequency)]
         times = np.linspace(self.border_left,
