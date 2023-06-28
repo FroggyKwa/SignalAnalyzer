d9bb1ee8b7f9e475351ec7d799f5a5016945c4c3
FroggyKwa <ilya.laguxa@gmail.com>
2023-06-29 02:50:28 +1000

    fix bug with right/left borders

--- application/plot_widget.py
+++ application/plot_widget.py
@@ -10,8 +10,10 @@ class MyPlotWidget(PlotWidget):
         super().__init__(**kwargs)
         self.mainwindow = mainwindow
         self.scene().sigMouseClicked.connect(self.mouse_clicked)
-        self.border_left: tuple[None | float, None | float] = (None, None)
-        self.border_right: tuple[None | float, None | float] = (None, None)
+        self.border_left = None
+        self.border_right = None
+        self.line_1 = None
+        self.line_2 = None
         self.plot_data = plot_data
         self.frequency = frequency
 
@@ -22,26 +24,34 @@ class MyPlotWidget(PlotWidget):
             mouse_point = vb.mapSceneToView(scene_coords)
             line = InfiniteLine(angle=90, movable=False, pen='r')
             line.setValue(mouse_point.x())
-            if not self.border_left[1]:
-                self.border_left = mouse_point.x(), line
-            elif not self.border_right[1]:
-                self.border_right = mouse_point.x(), line
+            if not self.border_left:
+                self.border_left = mouse_point.x()
+                self.line_1 = line
+            elif not self.border_right:
+                if self.border_left > mouse_point.x():
+                    self.border_right = self.border_left
+                    self.border_left = mouse_point.x()
+                else:
+                    self.border_right = mouse_point.x()
+                self.line_2 = line
                 self.open_scaled_plot()
-            elif self.border_right[1] and self.border_left[1]:
+            elif self.line_1 and self.line_2:
                 # delete borders from plot
-                self.removeItem(self.border_left[1])
-                self.removeItem(self.border_right[1])
+                self.removeItem(self.line_1)
+                self.removeItem(self.line_2)
 
-                self.border_left = mouse_point.x(), line
-                self.border_right = (None, None)
+                self.border_left = mouse_point.x()
+                self.line_1 = line
+                self.line_2 = None
+                self.border_right = None
 
             self.addItem(line)
             return mouse_point.x(), mouse_point.y()
 
     def open_scaled_plot(self):
-        print(self.border_left[0], self.border_right[0])
-        data = self.plot_data[1][int(self.border_left[0]*self.frequency):int(self.border_right[0]*self.frequency)]
-        times = np.linspace(self.border_left[0],
-                            self.border_right[0],
+        print(self.border_left, self.border_right)
+        data = self.plot_data[1][int(self.border_left * self.frequency):int(self.border_right * self.frequency)]
+        times = np.linspace(self.border_left,
+                            self.border_right,
                             num=len(data))
         d = pg.plot(times, data, pen='r')
